f024af1d545733e9d0907d5b49d21d4e13f211b6
hussainhyder23 <hussainhyder23@gmail.com>
2019-10-21 10:00:48 +0530

    Added the code for the ball

--- PingFest.py
+++ PingFest.py
@@ -22,6 +22,15 @@ paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350,0)
 
+#Ball
+ball=turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+ball.dx = 2
+ball.dy = -2
 
 #Function
 
